38651d43f026c6872969a6d2783b9d2c95133775
AHMED <med.informat@gmail.com>
2022-02-05 17:52:22 +0100

    new

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
-http
-.createServer((request, response) => {
-  fs.readFile("./index.html", "utf-8", (err, data) => {
-    response.end(data);
-  });
-}).listen("1337", "192.168.1.40");
+// http
+// .createServer((request, response) => {
+//   fs.readFile("./index.html", "utf-8", (err, data) => {
+//     response.end(data);
+//   });
+// }).listen("1337", "192.168.1.40");
+
+
+const app = express();
+app.listen(3000);
+
+app.get("/", (req, res, next) => {
+  fs.readFile(""./index.html")
+})
